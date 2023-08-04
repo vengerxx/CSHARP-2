87bf810b17e245ac4f6d0aacf1657d9d349783cf
Dimasik <pak.dima.2006@mail.ru>
2023-08-04 13:39:36 +0600

    Zadachka 2/2 seminar

---
+++ Seminar010/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+int numberOne = number / 100;
+Console.WriteLine(numberOne);
+
+int numberThree = number % 10;
+Console.WriteLine(numberThree);
+
+int result = numberOne*10 + numberThree;
+Console.WriteLine(result);
+
