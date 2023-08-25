13cfe7399307c361a53c197811db6b937f3683fe
Dimasik <pak.dima.2006@mail.ru>
2023-08-25 13:26:47 +0600

    zadachki posle kanikul

---
+++ Seminar014/Program.cs
@@ -0,0 +1,16 @@
+﻿// 24. Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+System.Console.WriteLine("Введите число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int SumOfNumber(int number)
+{
+    int sum = 0;
+    for(int i = 1; i <= userNumber; i++)
+{
+    sum += i;
+}
+    return sum;
+}
+
+Console.WriteLine($"Сумма цифр в числе {userNumber} равна {SumOfNumber(userNumber)}");
