7e887f4d4b582ba178f45e35ad00ffc0271ad09a
Dimasik <pak.dima.2006@mail.ru>
2023-08-25 13:42:44 +0600

    zadachki posle kanikul

---
+++ Seminar015/Program.cs
@@ -0,0 +1,19 @@
+﻿//26. Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+System.Console.WriteLine("Введите число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int Quantity(int number)
+{
+    int count = 0;
+    while (number>0)
+    {
+        count++;
+        number=number/10;
+    }
+    return count;
+}   
+Console.WriteLine(Quantity(userNumber));
