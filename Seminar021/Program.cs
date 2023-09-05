ac6b08014a6ec69dcdae7da3fe4ffe28506f6b83
Dimasik <pak.dima.2006@mail.ru>
2023-09-05 13:56:35 +0600

    zadachka 3/7 seminar

---
+++ Seminar021/Program.cs
@@ -0,0 +1,37 @@
+﻿//48. Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+Console.Clear();
+
+int[,] Get2DArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+     System.Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            System.Console.Write(arrayToPrint[i,j] +"\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int [,] arr = Get2DArray(5,10);
+Print2DArray(arr);
