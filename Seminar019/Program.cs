6794391dcc24a640529dfa4a62f47fa0e5785207
Dimasik <pak.dima.2006@mail.ru>
2023-09-05 13:37:11 +0600

    zadachka 1/7 seminar

---
+++ Seminar019/Program.cs
@@ -0,0 +1,13 @@
+﻿// 46. Задайте двумерный массив размером m×n, заполненный случайными целыми числами.|
+int[,] GetRandom2DArray(int rowNumber, int colNumber, int deviation)
+{
+    int[,] result = new int[rowNumber, colNumber];
+    for (int i = 0; i < rowNumber; i++)
+    {
+        for (int j = 0; j < colNumber; j++)
+        {
+            result[i,j] = new Random().Next(-deviation, deviation);
+        }
+    }
+    return result;
+}
