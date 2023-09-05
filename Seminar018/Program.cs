bd399ba88afe6d6c14fce3b3c2d2da82ce11bc6b
Dimasik <pak.dima.2006@mail.ru>
2023-09-05 13:33:31 +0600

    zadacha samoproverka

---
+++ Seminar018/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество чисел M: ");
+        if (!int.TryParse(Console.ReadLine(), out int m))
+        {
+            Console.WriteLine("Ошибка ввода. Пожалуйста, введите целое число.");
+            return;
+        }
+
+        int countPositiveNumbers = 0;
+
+        for (int i = 1; i <= m; i++)
+        {
+            Console.Write($"Введите число {i}: ");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                if (number > 0)
+                {
+                    countPositiveNumbers++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода. Пожалуйста, введите целое число.");
+                i--; // Повторяем ввод текущего числа.
+            }
+        }
+
+        Console.WriteLine($"Количество положительных чисел: {countPositiveNumbers}");
+    }
+}
